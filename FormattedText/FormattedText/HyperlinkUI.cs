cae3642aeb82c8a4156b812050120fc851eae3aa
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2022-02-23 17:58:48 +0530

    FormattedText sample added

---
+++ FormattedText/FormattedText/HyperlinkUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormattedText
+{
+    public class HyperlinkUI : Span
+    {
+        public static readonly BindableProperty LinkUrlProperty =
+            BindableProperty.Create(nameof(LinkUrl), typeof(string), typeof(HyperlinkUI), null);
+
+        public string LinkUrl
+        {
+            get
+            {
+                return (string)GetValue(LinkUrlProperty);
+            }
+            set
+            {
+                SetValue(LinkUrlProperty, value);
+            }
+        }
+
+        public HyperlinkUI()
+        {
+            ApplyHyperlinkAppearance();
+        }
+
+        void ApplyHyperlinkAppearance()
+        {
+            this.TextColor = Color.FromArgb("#0000EE");
+            this.TextDecorations = TextDecorations.Underline;
+        }
+
+        void CreateNavgigationCommand()
+        {
+            //... Since Span inherits GestureElement, you can add Gesture Recognizer to navigate using LinkUrl
+        }
+
+    }
+}
---
+++ FormattedText/FormattedText/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+
+namespace FormattedText;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+	public MainPage()
+	{
+		InitializeComponent();
+		this.BindingContext = this;
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+
+	}
+
+    private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+    {
+		
+    }
+}
+
